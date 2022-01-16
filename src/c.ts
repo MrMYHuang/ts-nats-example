8b213f2c107dc63cad7335cfe9de0977f6ef0782
Meng-Yuan Huang <myh@live.com>
2022-01-17 00:05:15 +0800

    * Use JetStream.

--- src/c.ts
+++ src/c.ts
@@ -1,12 +1,36 @@
 import * as nats from 'nats';
 
+const stream = 'stream';
+const subject = 'subject';
+const consumer = 'consumer';
+
+let nc: nats.NatsConnection | undefined;
+let jsm: nats.JetStreamManager | undefined;
+let js: nats.JetStreamClient | undefined;
 nats.connect({
     servers: ['localhost:4221', 'localhost:4222', 'localhost:4223',],
-}).then(nc => {
-    const sub = nc.subscribe('subject');
-    (async () => {
-        for await (const m of sub) {
-            console.log(nats.JSONCodec().decode(m.data));
-        }
-    })();
+}).then(_nc => {
+    nc = _nc;
+    js = nc.jetstream();
+    return nc.jetstreamManager();
+}).then(c => {
+    const opts = nats.consumerOpts({
+        durable_name: consumer,
+        ack_policy: nats.AckPolicy.Explicit,
+        deliver_subject: consumer,
+    });
+    //opts.durable(consumer);
+    opts.manualAck();
+    //opts.ackExplicit();
+    //opts.deliverTo(consumer);
+
+    return js!.subscribe(subject, opts)
+}).then(async (jss) => {
+
+    for await (const s of jss) {
+        console.log(nats.JSONCodec().decode(s.data));
+        s.ack();
+    }
+}).catch(error => {
+    console.error(error);
 });
--- src/p.ts
+++ src/p.ts
@@ -1,13 +1,33 @@
 import * as nats from 'nats';
 
+const stream = 'stream';
+const subject = 'subject';
+
+const message = process.argv[2] || 'Hello!';
+
+let nc: nats.NatsConnection | undefined;
+let jsm: nats.JetStreamManager | undefined;
+let js: nats.JetStreamClient | undefined;
 nats.connect({
     servers: ['localhost:4221', 'localhost:4222', 'localhost:4223',],
-}).then(nc => {
-    nc.publish('subject',
-        nats.JSONCodec().encode({ hello: 'world' })
-    );
-
+}).then(_nc => {
+    nc = _nc;
+    js = nc.jetstream();
+    return nc.jetstreamManager();
+}).then(_jsm => {
+    jsm = _jsm;
+    return jsm!.streams.add({
+        name: stream,
+        subjects: [subject],
+        no_ack: false,
+    });
+}).then(si => {
+    return js!.publish(subject,
+        nats.JSONCodec().encode({ hello: message }));
+}).then(pa => {
     setTimeout(() => {
         process.exit(0);
-    }, 500);
-});
+    }, 50);
+}).catch(error => {
+    console.error(error);
+})
